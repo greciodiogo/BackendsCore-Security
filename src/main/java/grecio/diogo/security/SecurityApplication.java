ab48796e276aaa117869aed9c6b53ef0a4ad7a44
Grécio Diogo <fonebahia8@gmail.com>
2024-09-05 01:02:42 +0100

    first commit

---
+++ src/main/java/grecio/diogo/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package grecio.diogo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grecio/diogo/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package grecio.diogo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
