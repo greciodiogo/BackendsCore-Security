ca107fecc4c56caabd642b4d8406e6c2cbc9b74d
Grécio Diogo <fonebahia8@gmail.com>
2024-09-17 23:58:40 +0100

    refactor: refactor module and add api-gateway

--- api-gateway/src/main/java/com/core/grecio/diogo/api_gateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/core/grecio/diogo/api_gateway/config/AppConfig.java
@@ -1,3 +1,4 @@
+package com.core.grecio.diogo.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/core/grecio/diogo/backend/DTO/PermissionFieldDTO.java
+++
@@ -1,8 +0,0 @@
-package com.core.grecio.diogo.backend.DTO;
-
-
-public record PermissionFieldDTO(
-    Integer id,
-    String slug,
-    Boolean is_deleted
-    ) {}
